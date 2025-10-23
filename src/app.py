40ff981d583240b9322afbd2e86902057f8a75ea
valeriacaro23 <valeringallego1@gmail.com>
2025-10-23 08:59:40 -0500

    inittial commit

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import os
+from Flask import Flask
+
+app = Flask(__name__)
+
+@app.route ('/')
+def index():
+    return 'Mi primer hola mundo'
+
+if __name__ =='__main__':
+    app.run(host='0.0.0.0', port=5000)
